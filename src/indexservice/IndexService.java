8df2c15f3ae92882d292a9c580d1ab8d1a053caa
Francisco I. Avello B <francisco.avello@usach.cl>
2016-01-24 20:39:03 -0300

    Ajustado IndexService para integración entre los servicios

--- src/indexservice/IndexService.java
+++ src/indexservice/IndexService.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,6 +19,57 @@ public class IndexService extends Thread{
         this.query = query;
     }
     
+    // Para recibir desde el FrontService
+    
+    public static void socketServidorIndexServiceParaFrontService() throws Exception{    
+        
+        //Variables
+        String desdeFrontService;        
+        //Socket para el servidor en el puerto
+        ServerSocket socketDesdeFrontService = new ServerSocket(5003);
+        
+        //Socket listo para recibir 
+        Socket connectionSocket = socketDesdeFrontService.accept();
+        //Buffer para recibir desde el cliente
+        BufferedReader inDesdeFrontService = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        //Buffer para enviar al cliente
+            
+        //Recibimos el dato del cliente y lo mostramos en el server
+        desdeFrontService =inDesdeFrontService.readLine();
+        System.out.println("Recibidos: " + desdeFrontService);
+        IndexService hilo = new IndexService(desdeFrontService);
+        hilo.start();
+        socketDesdeFrontService.close();
+    }
+        
+    // Para enviar al FrontService
+    
+    public static void socketClienteDesdeIndexServiceHaciaFrontService(String respuestaAFrontService) throws Exception{        
+        //Socket para el cliente (host, puerto)
+        Socket socketHaciaFrontService = new Socket("localhost", 5004);
+        
+        //Buffer para enviar el dato al server
+        DataOutputStream haciaElFrontService = new DataOutputStream(socketHaciaFrontService.getOutputStream());
+        
+        haciaElFrontService.writeBytes(respuestaAFrontService + '\n');
+        
+        socketHaciaFrontService.close();  
+    }
+    
+    // Para enviar al CachingService
+    
+    public static void socketClienteDesdeIndexServiceHaciaCachingService(String respuestaACachingService) throws Exception{        
+        //Socket para el cliente (host, puerto)
+        Socket socketHaciaCachingService = new Socket("localhost", 5005);
+        
+        //Buffer para enviar el dato al server
+        DataOutputStream haciaElCachingService = new DataOutputStream(socketHaciaCachingService.getOutputStream());
+        
+        haciaElCachingService.writeBytes(respuestaACachingService + '\n');
+        
+        socketHaciaCachingService.close();  
+    }
+    
     @Override
     public void run(){        
         try {                   
@@ -25,22 +78,37 @@ public class IndexService extends Thread{
             DBCollection tablaDatos = db.getCollection("datosWiki");    
             DBCollection indiceInvertido = db.getCollection("indiceInvertido");
             System.out.println("(Index Service) Soy el thread: " + getName() + ". Recibi la query '" + query + "'. Buscando en el indice invertido");
-            System.out.println("Buscado en el índice invertido...");
+            System.out.println("(Index Service) Buscado en el índice invertido...");
             DBObject queryDB = new BasicDBObject("palabra", query);
             DBCursor cursor = indiceInvertido.find(queryDB);
             if(cursor.hasNext()){ // Se busca la palabra en el indice invertido
                 DBObject objetoPresente = cursor.next();
                 IndexInvertido ind = new IndexInvertido((BasicDBObject) objetoPresente);
+                String paraEnviar= new String();
                 for (int i = 0; i < ind.docFrec.size(); i++) {
                     System.out.println("--------------------");
                     System.out.println("Está en el documento: " + ind.docFrec.get(i).idDocumento);
                     System.out.println("Con una frecuencia de: " + ind.docFrec.get(i).frecuencia);
                     System.out.println("En la URL de wikipedia: " + obtenerURL(ind.docFrec.get(i).idDocumento,mongo));
+                    paraEnviar = paraEnviar.concat(ind.docFrec.get(i).idDocumento);
+                    paraEnviar = paraEnviar.concat("#");
+                    paraEnviar = paraEnviar.concat(ind.docFrec.get(i).frecuencia.toString());
+                    paraEnviar = paraEnviar.concat("#");
+                    paraEnviar = paraEnviar.concat(obtenerURL(ind.docFrec.get(i).idDocumento,mongo));
+                    if(i != ind.docFrec.size()-1){
+                        paraEnviar = paraEnviar.concat(",");
+                    }                    
                 }
-                
+                String paraEnviarCaching = paraEnviar.concat(","+query); 
+                socketClienteDesdeIndexServiceHaciaCachingService(paraEnviarCaching);
+                socketClienteDesdeIndexServiceHaciaFrontService(paraEnviar);
             }
             else{ // No existe en el indice
-                System.out.println("No se han encontrado resultados en el indice invertido");
+                System.out.println("(Index Service) No se han encontrado resultados en el indice invertido");
+                String respuestaAFrontService = "MISS!!";
+                socketClienteDesdeIndexServiceHaciaCachingService("NO");
+                socketClienteDesdeIndexServiceHaciaFrontService(respuestaAFrontService);
+                
             }
             System.out.println("");
         } catch (Exception ex) {
@@ -62,13 +130,9 @@ public class IndexService extends Thread{
         return URL;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Exception {
         while(true){
-            String query;
-            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-            query = inFromUser.readLine();
-            IndexService hilo = new IndexService(query);
-            hilo.start();
+            socketServidorIndexServiceParaFrontService();            
         }
     }
     
